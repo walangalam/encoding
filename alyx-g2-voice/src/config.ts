dfa359253c865d5998aa40fe619c1ea31c80376e
walangalam <rsmurao@slu.edu.ph>
2026-08-13 19:16:47 +0800

    Add canonical Alyx G2 Voice v0.2.2 config

---
+++ alyx-g2-voice/src/config.ts
@@ -0,0 +1,6 @@
+export const BRIDGE_WS_URL = 'wss://ramurgamefix.com/alyx-g2-voice/ws'
+export const BRIDGE_TOKEN = 'qVAyXcliAXtumjSIKPOdzPkCw1qCKV6yANTitDEvizo'
+export const WAKE_WORD = 'Alyx'
+export const SILENCE_MS = 1350
+export const VOICE_DB_THRESHOLD = -42
+export const RECONNECT_MS = 2500
